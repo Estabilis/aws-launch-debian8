042415228b8ca36616023337b70c8977c4d415f5
Daniel Gines <daniel@estabilis.com>
2019-08-04 11:42:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 echo "copiando o projeto"
+sudo apt-get install git -y 
 sudo git clone https://github.com/Estabilis/aws-launch-debian8.git
 echo "atualizando o repositorio do Debian 8 - Jessie"
 sudo rm /etc/apt/sources.list.d/backports.list
 sudo rm /etc/apt/sources.list
-sudo cp aws-launch-debian8/unix-conf/sources-list.conf /etc/apt/sources.list 
+sudo cp aws-launch-debian8/unix-conf/sources-lists.conf /etc/apt/sources.list 
 sudo apt-get update
 echo "atualizando o sistema"
 sudo apt-get dist-upgrade -y
@@ -24,12 +25,12 @@ sudo dpkg -i zabbix-release_4.0-3+jessie_all.deb
 sudo apt update
 sudo apt -y install zabbix-agent
 echo "atualizar o arquivo de configuracoes do zabbix-agent"
-sudo cat aws-launch-debian8/zabbix-conf/zabbix_agentd.conf >> /etc/zabbix/zabbix_agentd.conf
+sudo bash -c 'cat aws-launch-debian8/zabbix-conf/zabbix_agentd.conf > /etc/zabbix/zabbix_agentd.conf'
 echo "ajustes no .bashrc - history"
-sudo cat aws-launch-debian8/unix-conf/bashrc.conf >> /root/.bashrc
-sudo cat aws-launch-debian8/unix-conf/bashrc.conf >> /home/admin/.bashrc
-sudo cat aws-launch-debian8/unix-conf/bashrc.conf >> /etc/skel/.bashrc
+sudo bash -c 'cat aws-launch-debian8/unix-conf/bashrc.conf >> /root/.bashrc'
+sudo bash -c 'cat aws-launch-debian8/unix-conf/bashrc.conf >> /home/admin/.bashrc'
+sudo bash -c 'cat aws-launch-debian8/unix-conf/bashrc.conf >> /etc/skel/.bashrc'
 echo "alterando o timezone para America/Sao_Paulo"
 # fonte - https://www.illucit.com/en/linux/timezone-in-debian-9-stretch/
-sudo echo "America/Sao_Paulo" > /etc/timezone
+sudo bash -c 'echo "America/Sao_Paulo" > /etc/timezone'
 sudo dpkg-reconfigure -f noninteractive tzdata
